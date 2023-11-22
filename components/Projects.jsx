fed38f5a1f828602c54b87e8db853a6b2c1eff90
KudaMukucha <kudamukucha80@gmail.com>
2023-11-22 08:07:01 +0200

    discover image fix

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import ProjectItem from "./ProjectItem"
-import DiscoverImg from '../public/discover.png'
+import DiscoverImg from '../public/discover.PNG'
 
 const Projects = () => {
   return (
